a7f194ab04f3d237f50eb50604c2d64370b81327
Andrei Miron <victormiron.home@gmail.com>
2024-06-04 12:08:57 -0400

    needs revised

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,9 +1,7 @@
 SELECT customers.customer_name,
-       subscriptions.price_per_month,
-       subscriptions.subscription_length,
-       (subscriptions.subscription_length * subscriptions.price_per_month) AS Amount_Due
+       SUM(subscriptions.subscription_length * subscriptions.price_per_month) AS Amount_Due
 FROM orders
 INNER JOIN customers ON customers.customer_id = orders.customer_id
 INNER JOIN subscriptions ON orders.subscription_id = subscriptions.subscription_id
 WHERE order_status = 'unpaid'
-GROUP BY customer_name;
+GROUP BY customers.customer_name;
